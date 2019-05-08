0b4f646a3f2deaeb327f7a3f74ee5094c2e7315c
Bryce Jech <bryce@brycejech.com>
2019-05-08 09:57:47 -0500

    add store methods for bookings and details

--- client/src/store.js
+++ client/src/store.js
@@ -3,16 +3,73 @@
 import Vue  from 'vue';
 import Vuex from 'vuex';
 
+import api from './api';
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
+const store = new Vuex.Store({
+    state: {
+        bookings: [],
+        bookingDetails: []
+    },
+    getters: {
+        bookings(state){
+            return state.bookings;
+        },
+        booking: state => id => {
+            return state.bookings.filter(booking => booking.id === id)[0];
+        },
+        bookingDetails(state){
+            return state.bookingDetails;
+        },
+        bookingDetail: state => bookingId => {
+            return state.bookingDetails.filter(booking => booking.bookingId == bookingId)[0];
+        }
+    },
+    mutations: {
+        addBooking(state, booking){
+            const existing = this.getters.booking(booking.id);
 
-  },
-  mutations: {
+            if(existing){
+                const idx = this.getters.bookings.indexOf(existing);
 
-  },
-  actions: {
+                if(idx >= 0){
+                    state.bookings.splice(idx, 1);
+                }
+            }
+            state.bookings.push(booking);
+        },
+        addBookingDetail(state, detail){
+            const existing = this.getters.bookingDetail(detail.bookingId);
 
-  }
+            if(existing){
+                const idx = this.getters.bookingDetails.indexOf(detail);
+
+                if(idx >= 0){
+                    state.bookingDetails.splice(idx, 1);
+                }
+            }
+            state.bookingDetails.push(detail);
+        }
+    },
+    actions: {
+        async loadBookings({ commit, dispatch }){
+            const bookings = await api.getBookings();
+
+            bookings.forEach(booking => {
+                commit('addBooking', booking);
+
+                dispatch('loadBookingDetail', booking);
+            });
+        },
+        async loadBookingDetail({ commit }, booking){
+            const bookingDetail = await api.getBookingDetail(booking.id);
+
+            commit('addBookingDetail', bookingDetail);
+        }
+    }
 });
+
+store.dispatch('loadBookings');
+
+export default store;
