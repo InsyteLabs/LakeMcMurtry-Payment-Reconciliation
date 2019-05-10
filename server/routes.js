743771a316474c0ae029a3c43c0810c7da4cff4c
Bryce Jech <bryce@brycejech.com>
2019-05-09 20:49:06 -0500

    add route to get Checkfront items

--- server/routes.js
+++ server/routes.js
@@ -4,14 +4,15 @@ const router = require('express').Router();
 
 const checkfrontClient = require('./Http/CheckfrontClient'),
       Booking          = require('./Models/Booking'),
-      BookingDetail    = require('./Models/BookingDetail');
+      BookingDetail    = require('./Models/BookingDetail'),
+      Item             = require('./Models/Item');
 
 router.get('/bookings', async (req, res, next) => {
     let bookings = await checkfrontClient.getBookings();
 
     bookings = Object.keys(bookings['booking/index'])
-                .map(key => bookings['booking/index'][key])
-                .map(booking => new Booking(booking));
+        .map(key => bookings['booking/index'][key])
+        .map(booking => new Booking(booking));
 
     return res.json(bookings);
 });
@@ -24,6 +25,19 @@ router.get('/bookings/:id', async (req, res, next) => {
     if(!detail) return res.status(404).json({ message: 'Not found' });
 
     return res.json(new BookingDetail(detail));
+});
+
+router.get('/items', async (req, res, next) => {
+    let items = (await checkfrontClient.getItems()).items;
+
+    items = Object.keys(items)
+        .map(key => new Item(items[key]))
+        .sort((a, b) => {
+            if(a.category === b.category) return 0;
+            return a.category > b.category ? 1 : -1;
+        });
+
+    return res.json(items);
 });
 
 module.exports = router;
