1105e7883c8f1551227878c4f93d326bf0b13e32
Bryce Jech <bryce@brycejech.com>
2019-05-09 21:22:24 -0500

    update item sort method

--- server/routes.js
+++ server/routes.js
@@ -32,8 +32,22 @@ router.get('/items', async (req, res, next) => {
 
     items = Object.keys(items)
         .map(key => new Item(items[key]))
+        // Sort by category, then by name
         .sort((a, b) => {
-            if(a.category === b.category) return 0;
+            if(a.category === b.category){
+                /*
+                    Pick and sort number-like names like "Site #1"
+                    Ensures that "Site #10" comes at the end, not right after
+                    "Site #1"
+                */
+                if(/[0-9]/.test(a.name) && /[0-9]/.test(b.name)){
+                    const aName = parseInt(a.name.replace(/\D/g, '')),
+                          bName = parseInt(b.name.replace(/\D/g, ''));
+
+                    return aName - bName;
+                }
+                return a.name > b.name ? 1 : -1;
+            }
             return a.category > b.category ? 1 : -1;
         });
 
