d072086d7b694caae31cd0c7da7203c30ff56f39
Bryce Jech <bryce@brycejech.com>
2019-05-18 09:58:29 -0500

    recursive getBookings, move methods outside of exports

--- server/Http/CheckfrontClient.js
+++ server/Http/CheckfrontClient.js
@@ -5,77 +5,105 @@ const axios = require('axios'),
 
 axios.defaults.headers.common['Authorization'] = `Basic ${ conf.token }`;
 
-module.exports = {
-    async getAccount(){
-        try{
-            const res = await axios({
-                method: 'get',
-                url: `${ conf.endpoint }/account`,
-                responseType: 'json'
-            });
+async function getAccount(){
+    try{
+        const res = await axios({
+            method: 'get',
+            url: `${ conf.endpoint }/account`,
+            responseType: 'json'
+        });
 
-            return res.data
-        }
-        catch(e){ throw e }
-    },
-    async getItems(){
-        try{
-            const res = await axios({
-                method: 'get',
-                url: `${ conf.endpoint }/item`,
-                responseType: 'json'
-            });
-
-            return res.data;
-        }
-        catch(e){ throw e }
-    },
-    async getItem(id){
-        try{
-            const res = await axios({
-                method: 'get',
-                url: `${ conf.endpoint }/item/${ id }`,
-                responseType: 'json'
-            });
-
-            return res.data;
-        }
-        catch(e){ throw e }
-    },
-    async getEvents(){
-        try{
-            const res = await axios({
-                method: 'get',
-                url: `${ conf.endpoint }/item`,
-                responseType: 'json'
-            });
-
-            return res.data;
-        }
-        catch(e){ throw e }
-    },
-    async getBookings(){
-        try {
-            const res = await axios({
-                method: 'get',
-                url: `${ conf.endpoint }/booking?created_date=${ '>1553144400' }`,
-                responseType: 'json'
-            });
-
-            return res.data;
-        }
-        catch(e){ throw e }
-    },
-    async getBooking(id){
-        try{
-            const res = await axios({
-                method: 'get',
-                url: `${ conf.endpoint }/booking/${ id }`,
-                responseType: 'json'
-            });
-
-            return res.data;
-        }
-        catch(e){ throw e }
+        return res.data
     }
+    catch(e){ throw e }
+}
+
+async function getItems(){
+    try{
+        const res = await axios({
+            method: 'get',
+            url: `${ conf.endpoint }/item`,
+            responseType: 'json'
+        });
+
+        return res.data;
+    }
+    catch(e){ throw e }
+}
+
+async function getItem(id){
+    try{
+        const res = await axios({
+            method: 'get',
+            url: `${ conf.endpoint }/item/${ id }`,
+            responseType: 'json'
+        });
+
+        return res.data;
+    }
+    catch(e){ throw e }
+}
+
+async function getEvents(){
+    try{
+        const res = await axios({
+            method: 'get',
+            url: `${ conf.endpoint }/item`,
+            responseType: 'json'
+        });
+
+        return res.data;
+    }
+    catch(e){ throw e }
+}
+
+/*
+    GET BOOKINGS
+    ------------
+    Recursively calls itself until all pages are consumed
+*/
+async function getBookings(page=1, bookings=[]){
+    try {
+        const res = await axios({
+            method: 'get',
+            url: `${ conf.endpoint }/booking?created_date=${ '>1546322400' }&limit=250&page=${ page }`,
+            responseType: 'json'
+        });
+
+        const newBookings = Object.keys(res.data['booking/index'])
+            .map(key => res.data['booking/index'][key]);
+
+        bookings = [...bookings, ...newBookings];
+
+        console.log(res.data.request);
+
+        if(res.data.request.page < res.data.request.pages){
+            return await getBookings(res.data.request.page + 1, bookings);
+        }
+
+        return bookings;
+    }
+    catch(e){ throw e }
+}
+
+async function getBooking(id){
+    try{
+        const res = await axios({
+            method: 'get',
+            url: `${ conf.endpoint }/booking/${ id }`,
+            responseType: 'json'
+        });
+
+        return res.data;
+    }
+    catch(e){ throw e }
+}
+
+module.exports = {
+    getAccount,
+    getItems,
+    getItem,
+    getEvents,
+    getBookings,
+    getBooking
 }
