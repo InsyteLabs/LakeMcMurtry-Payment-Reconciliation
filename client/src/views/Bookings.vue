37d5100de09d69d76fe1c29d51e65ff0cae73ebf
Bryce Jech <bryce@brycejech.com>
2019-05-19 11:36:04 -0500

    add transaction table, scroll tables, show counts

--- client/src/views/Bookings.vue
+++ client/src/views/Bookings.vue
@@ -1,8 +1,7 @@
 <template lang="html">
     <div class="bookings container">
-        <h1>Bookings</h1>
-        <div class="row">
-            <div class="col-2">
+        <div class="row filter-row">
+            <div class="col-2 pr-0">
                 <select id="month" v-model="month">
                     <option value="1">January</option>
                     <option value="2">February</option>
@@ -18,7 +17,7 @@
                     <option value="12">December</option>
                 </select>
             </div>
-            <div class="col-2">
+            <div class="col-2 pr-0">
                 <select id="year" v-model="year">
                     <option value="2012">2012</option>
                     <option value="2013">2013</option>
@@ -34,42 +33,74 @@
                     <option value="2023">2023</option>
                 </select>
             </div>
-            <div class="col-1">
+            <div class="col-1 pr-0">
                 <button @click="loadBookings" class="bg-green-dark text-yellow-light btn-block" id="filter-btn" type="button" role="button">FILTER</button>
             </div>
         </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Created</th>
-                    <th>Start</th>
-                    <th>End</th>
-                    <th style="max-width: 150px">Summary</th>
-                    <th>Status</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Total</th>
-                    <th>Paid</th>
-                    <th>View</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="booking in bookings">
-                    <td class="no-wrap">{{ formatDate(booking.created) }}</td>
-                    <td>{{ booking.startDate }}</td>
-                    <td>{{ booking.endDate }}</td>
-                    <td class="summary"><div v-html="formatSummary(booking.summary)"></div></td>
-                    <td>{{ booking.status }}</td>
-                    <td>{{ booking.customer.name }}</td>
-                    <td>{{ booking.customer.email }}</td>
-                    <td>${{ booking.total.toFixed(2) }}</td>
-                    <td>${{ booking.paidTotal.toFixed(2) }}</td>
-                    <td>
-                        <button @click="viewDetail(booking.id)" class="btn-block bg-green-dark text-yellow-light" type="button" role="button">View</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+
+
+        <h1>Showing {{ bookings.length }} bookings created in {{ months[selectedMonth] }} {{ year }}</h1>
+        <div class="table-scroll mb-5">
+            <table class="mb-0">
+                <thead>
+                    <tr>
+                        <th>Created</th>
+                        <th>Start</th>
+                        <th>End</th>
+                        <th style="max-width: 150px">Summary</th>
+                        <th>Status</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Total</th>
+                        <th>Paid</th>
+                        <th>View</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="booking of bookings">
+                        <td class="no-wrap">{{ formatDate(booking.created) }}</td>
+                        <td>{{ booking.startDate }}</td>
+                        <td>{{ booking.endDate }}</td>
+                        <td class="summary"><div v-html="formatSummary(booking.summary)"></div></td>
+                        <td>{{ booking.status }}</td>
+                        <td>{{ booking.customer.name }}</td>
+                        <td>{{ booking.customer.email }}</td>
+                        <td>${{ booking.total.toFixed(2) }}</td>
+                        <td>${{ booking.paidTotal.toFixed(2) }}</td>
+                        <td>
+                            <button @click="viewDetail(booking.id)" class="btn-block bg-green-dark text-yellow-light" type="button" role="button">View</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+        <h1>Showing {{ transactions.length }} transactions in {{ months[selectedMonth] }} {{ year }}</h1>
+        <div class="table-scroll">
+            <table class="mb-0">
+                <thead>
+                    <tr>
+                        <th>Booking</th>
+                        <th>Transaction ID</th>
+                        <th>Transaction Date</th>
+                        <th>Status</th>
+                        <th>Amount</th>
+                        <th>Gateway</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="transaction of transactions" :class="{ refund: transaction.status === 'REFUND' }">
+                        <td>{{ transaction.bookingId }}</td>
+                        <td>{{ transaction.id }}</td>
+                        <td>{{ transaction.date }}</td>
+                        <td>{{ transaction.status }}</td>
+                        <td>{{ transaction.amount }}</td>
+                        <td>{{ transaction.gateway }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 
 </template>
@@ -88,7 +119,22 @@ export default {
     data(){
         return {
             month,
-            year
+            selectedMonth: month,
+            year,
+            months: {
+                1:  'January',
+                2:  'February',
+                3:  'March',
+                4:  'April',
+                5:  'May',
+                6:  'June',
+                7:  'July',
+                8:  'August',
+                9:  'September',
+                10: 'October',
+                11: 'November',
+                12: 'December'
+            }
         }
     },
     computed: {
@@ -101,6 +147,8 @@ export default {
     },
     methods: {
         loadBookings(){
+            this.selectedMonth = this.month;
+
             this.$store.dispatch('loadBookings', {
                 month: this.month,
                 year:  this.year
@@ -135,4 +183,17 @@ export default {
 
 #filter-btn
     margin-top: 10px
+
+.table-scroll
+    max-height: 450px
+    overflow-y: auto
+    margin-bottom: 50px
+
+.filter-row
+    margin-top: -50px
+    margin-bottom: 50px
+
+.refund
+    color: red
+
 </style>
