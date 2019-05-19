6aba41fc59f1d0fc8fa36c650a436846cb8e4c75
Bryce Jech <bryce@brycejech.com>
2019-05-19 13:18:59 -0500

    map items within client, add getCategories method

--- server/Http/CheckfrontClient.js
+++ server/Http/CheckfrontClient.js
@@ -3,6 +3,8 @@
 const axios = require('axios'),
       conf  = require('../conf').checkfront;
 
+const Item = require('../Models/Item');
+
 axios.defaults.headers.common['Authorization'] = `Basic ${ conf.token }`;
 
 async function getAccount(){
@@ -26,9 +28,50 @@ async function getItems(){
             responseType: 'json'
         });
 
-        return res.data;
+        let items = res.data.items;
+
+        items = Object.keys(items)
+            .map(key => new Item(items[key]))
+            // Sort by category, then by name
+            .sort((a, b) => {
+                if(a.category === b.category){
+                    /*
+                        Pick and sort number-like names like "Site #1"
+                        Ensures that "Site #10" comes at the end, not right after
+                        "Site #1"
+                    */
+                    if(/[0-9]/.test(a.name) && /[0-9]/.test(b.name)){
+                        const aName = parseInt(a.name.replace(/\D/g, '')),
+                              bName = parseInt(b.name.replace(/\D/g, ''));
+
+                        return aName - bName;
+                    }
+                    return a.name > b.name ? 1 : -1;
+                }
+                return a.category > b.category ? 1 : -1;
+            });
+
+        return items;
     }
     catch(e){ throw e }
+}
+
+async function getCategories(){
+    const items = await getItems();
+
+    const ids = [], categories = [];
+
+    items.forEach(item => {
+        if(!~ids.indexOf(item.categoryId)){
+            ids.push(item.categoryId);
+            categories.push({
+                id: item.categoryId,
+                name: item.category
+            });
+        }
+    });
+
+    return categories;
 }
 
 async function getItem(id){
@@ -124,6 +167,7 @@ module.exports = {
     getAccount,
     getItems,
     getItem,
+    getCategories,
     getEvents,
     getBookings,
     getBooking
