06f585b777bdcca21080291fcb1b0ad73ca9703d
Bryce Jech <bryce@brycejech.com>
2019-05-26 10:20:29 -0500

    simplify settlement route, add items-by-category & categories

--- server/routes.js
+++ server/routes.js
@@ -19,91 +19,33 @@ router.get('/bookings', async (req, res, next) => {
 
     let bookings = await checkfrontClient.getBookings(month, year);
 
-    bookings = bookings.map(booking => new Booking(booking));
-
     return res.json(bookings);
 });
+
+router.get('/bookings-with-details', async (req, res, next) => {
+    let { month, year } = req.query;
+
+    const details = await checkfrontClient.getBookingsWithDetails(month, year);
+
+    return res.json(details);
+})
 
 router.get('/bookings/:id', async (req, res, next) => {
     const bookingId = req.params.id;
 
-    let detail = (await checkfrontClient.getBooking(bookingId)).booking;
+    let detail = await checkfrontClient.getBooking(bookingId);
 
     if(!detail) return res.status(404).json({ message: 'Not found' });
 
-    return res.json(new BookingDetail(detail));
+    return res.json(detail);
 });
 
 router.get('/settlement', async (req, res, next) => {
 
-    const itemsAvailable      = await checkfrontClient.getItems(),
-          categoriesAvailable = await checkfrontClient.getCategories();
+    const { month, year } = req.query;
+    const settlement = await checkfrontClient.getSettlementTransactions(month, year);
 
-    const categoryMap = categoriesAvailable.reduce((map, cat) => {
-        map[cat.id] = cat.name;
-        return map;
-    }, {});
-
-    let { month, year } = req.query;
-
-    if(!(month && year)){
-        const date = new Date();
-
-        month = date.getMonth() + 1;
-        year  = date.getFullYear();
-    }
-
-    let bookings = await checkfrontClient.getBookings(month, year);
-
-    bookings = bookings.map(booking => new Booking(booking));
-
-    const promises     = [],
-          allTransactions = [];
-
-    bookings.forEach(async booking => {
-        promises.push(new Promise(async (resolve, reject) => {
-            let detail = (await checkfrontClient.getBooking(booking.id)).booking;
-
-            let transactions = Object.keys(detail.transactions).map(key => detail.transactions[key]),
-                items        = Object.keys(detail.items).map(key => detail.items[key]);
-
-            const bookedItems      = new Set(),
-                  bookedCategories = new Set();
-
-            transactions.forEach(transaction => {
-
-                items.forEach(item => {
-                    bookedItems.add(item.name);
-                    bookedCategories.add(categoryMap[item.category_id]);
-                });
-
-                allTransactions.push({
-                        id:             transaction.id,
-                        status:         transaction.status,
-                        date:           new Date(transaction.date * 1000),
-                        amount:         Number(transaction.amount),
-                        gateway:        transaction.gateway_id,
-                        bookingId:      booking.id,
-                        bookingCode:    booking.code,
-                        bookingStatus:  booking.statusId,
-                        bookingSummary: booking.summary,
-
-                        items:         [...bookedItems],
-                        multipleItems: bookedItems.size > 1,
-
-                        categories:         [...bookedCategories],
-                        multipleCategories: bookedCategories.size > 1
-                });
-            });
-
-            resolve();
-        }));
-    });
-
-    Promise.all(promises).then(() => {
-        return res.json(allTransactions);
-    }).catch(e => console.log(e));
-
+    return res.json(settlement);
 });
 
 router.get('/items', async (req, res, next) => {
@@ -116,6 +58,18 @@ router.get('/items/:id', async (req, res, next) => {
     let item = await checkfrontClient.getItem(req.params.id);
 
     return res.json(item.item);
+});
+
+router.get('/items-by-category', async (req, res, next) => {
+    let items = await checkfrontClient.getItemsByCategory();
+
+    return res.json(items);
+});
+
+router.get('/categories', async (req, res, next) => {
+    let categories = await checkfrontClient.getCategories();
+
+    return res.json(categories);
 });
 
 module.exports = router;
