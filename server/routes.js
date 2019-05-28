21d20e2bdd316d4e89ee5a76295a0454f7b7efce
Bryce Jech <bryce@brycejech.com>
2019-05-28 07:19:19 -0500

    group and total transactions

--- server/routes.js
+++ server/routes.js
@@ -48,6 +48,89 @@ router.get('/settlement', async (req, res, next) => {
     return res.json(settlement);
 });
 
+router.get('/settlement-alt', async (req, res, next) => {
+    const { month, year } = req.query;
+    const settlement = await checkfrontClient.getSettlementTransactions(month, year);
+
+    const categoryMap = {
+        'East Tent Campsites': 'Tent Camping',
+        'West Tent Campsites': 'Tent Camping',
+
+        'Primitive Campsites': 'Primitive Camping',
+
+        'East RV Campsites': 'RV Camping',
+        'West RV Campsites': 'RV Camping',
+
+        'Annual Membership': 'Annual Memberships',
+        'Day Permit':        'Day Permits',
+
+        'Pavilion Rental': 'Pavilion Rentals'
+    }
+
+    const noCategories       = { category: 'No Categories',       transactions: [] },
+          multipleCategories = { category: 'Multiple Categories', transactions: [] };
+
+    const transactionsByCategory = [ noCategories, multipleCategories ];
+
+    const data = {
+        total: 0,
+        transactionCount: 0,
+        transactionsByCategory: {
+            'No Categories': {
+                total:            0,
+                transactionCount: 0,
+                transactions:     []
+            },
+            'Multiple Categories': {
+                total:            0,
+                transactionCount: 0,
+                transactions:     []
+            }
+        }
+    }
+
+    settlement.forEach(transaction => {
+
+        data.transactionCount++;
+        if(transaction.refund){
+            data.total -= transaction.amount;
+        }
+        else{
+            data.total += transaction.amount;
+        }
+
+        const transactionCategory = transaction.categories[0];
+        let transactionGroup;
+        if(transaction.multipleCategories){
+            transactionGroup = data.transactionsByCategory['Multiple Categories'];
+        }
+        else if(!transaction.categories.length){
+            transactionGroup = data.transactionsByCategory['No Categories'];
+        }
+        else if(data.transactionsByCategory[transactionCategory]){
+            transactionGroup = data.transactionsByCategory[transactionCategory];
+        }
+        else{
+            transactionGroup = data.transactionsByCategory[transactionCategory] = {
+                total: 0,
+                transactionCount: 0,
+                transactions: []
+            }
+        }
+
+        transactionGroup.transactionCount++;
+        if(transaction.refund){
+            transactionGroup.total -= transaction.amount;
+        }
+        else{
+            transactionGroup.total += transaction.amount;
+        }
+        transactionGroup.transactions.push(transaction);
+    });
+
+    return res.json(data);
+});
+
 router.get('/items', async (req, res, next) => {
     let items = await checkfrontClient.getItems();
 
