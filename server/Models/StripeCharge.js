ed1349ea4901f0c4341a78f9440f5459bc01460d
Bryce Jech <bryce@brycejech.com>
2019-05-30 23:07:41 -0500

    add models for Stripe transactions

---
+++ server/Models/StripeCharge.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function StripeCharge(charge){
+    try{
+        this.id          = charge.id;
+        this.bookingCode = charge.balance_transaction.description.replace(/-\d+$/, '');
+        this.status      = charge.status;
+        this.paid        = charge.paid;
+
+        this.amount         = parseFloat((charge.amount / 100).toFixed(2));
+        this.fee            = parseFloat((charge.balance_transaction.fee / 100).toFixed(2));
+        this.net            = parseFloat((charge.balance_transaction.net / 100).toFixed(2));
+        this.refunded       = charge.refunded;
+        this.amountRefunded = parseFloat((charge.amount_refunded / 100).toFixed(2));
+        this.currency       = charge.currency;
+
+        this.cardHolderName = charge.card.name;
+
+        this.transactionDate = new Date(charge.created * 1000);
+
+        this.receiptURL = charge.receipt_url;
+
+        return this;
+    }
+    catch(e){
+        console.log('FUCKING ERROR');
+        console.log(e.message);
+        console.log(charge);
+        console.log('----------------');
+    }
+}
+
+module.exports = StripeCharge;
---
+++ server/Models/StripeRefund.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function StripeRefund(refund){
+    this.id          = refund.id;
+    this.bookingCode = refund.charge.description.replace(/-\d+$/, '');
+    this.status      = refund.status;
+
+    this.amount = parseFloat((refund.amount / 100).toFixed(2));
+
+    this.cardHolderName = refund.charge.card.name;
+
+    this.transactionDate = new Date(refund.created * 1000);
+}
+
+module.exports = StripeRefund;
